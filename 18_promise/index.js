a51961b53127e3ccfb1c89b7865f886e978bd5c0
mukesh yadav <ydvmukesh72@gmail.com>
2024-07-29 09:21:44 +0530

    last commit

--- 18_promise/index.js
+++ 18_promise/index.js
@@ -1,53 +1,55 @@
 const firstPromiese = new Promise(function (resolve, reject) {
   setTimeout(function () {
     console.log("Acync is completed");
-    resolve()
+    resolve();
   }, 2000);
 });
-firstPromiese.then(function(){
-    console.log('promise consumed')
-})
+firstPromiese.then(function () {
+  console.log("promise consumed");  
+});
 
-const promiseThree = new Promise(function(resolve, reject){
-    setTimeout(()=>{
-resolve({email:'mason@gmail.com', name: 'mason'})
-    },2000)
-})
-promiseThree.then(function(user){
-console.log(user);
-})
+const promiseThree = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve({ email: "mason@gmail.com", name: "mason" });
+  }, 2000);
+});
+promiseThree.then(function (user) {
+  console.log(user);
+});
 
-const promiseFour = new Promise (function(resolve, reject){
- setTimeout(()=>{
-let error = true
-if(!error){
-    resolve({username:"Mukesh", password:'123'})
-}
-else{
-    reject('error: somthing went worng')
-}
- },3000)
-})
-promiseFour.then((user)=>{
-console.log(user);
-return user.username
-}).then((username)=>{
-console.log(username)
-}).catch((err)=>{
+const promiseFour = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+    if (!error) {
+      resolve({ username: "Mukesh", password: "123" });
+    } else {
+      reject("error: somthing went worng");
+    }
+  }, 3000);
+});
+promiseFour
+  .then((user) => {
+    console.log(user);
+    return user.username;
+  })
+  .then((username) => {
+    console.log(username);
+  })
+  .catch((err) => {
     console.log(err);
-}).finally(()=>{
-    console.log('our promise is either resolved or rejected');
-})
-
+  })
+  .finally(() => {
+    console.log("our promise is either resolved or rejected");
+  });
 
 // fetch
-fetch('https://jsonplaceholder.typicode.com/users')
-.then((response)=>{ 
-    return response.json
-})
-.then((data)=>{
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    return response.json;
+  })
+  .then((data) => {
     console.log(data);
-})
-.catch((err)=>{
-console.log(err);
-})
+  })
+  .catch((err) => {
+    console.log(err);
+  });
